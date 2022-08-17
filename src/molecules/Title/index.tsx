88de895fdd1df2f3936116200b7bbef4dd5eb9ee
Chady Chaito <chady.chaito@cashme.com.br>
2022-08-17 00:07:59 -0300

    ✨ (Title): Created home title as a molecule

---
+++ src/molecules/Title/index.tsx
@@ -0,0 +1,13 @@
+import * as S from "./styles";
+
+export const Title = () => {
+  return (
+    <S.Wrapper>
+      <S.Title>
+        Stockholm, <br />
+        Sweden
+      </S.Title>
+      <S.Subtitle>Tue, Jun 30</S.Subtitle>
+    </S.Wrapper>
+  );
+};
---
+++ src/molecules/Title/styles.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0.5rem 1.125rem;
+  gap: 0.375rem;
+`;
+
+export const Title = styled.h1`
+  font-weight: 500;
+  font-size: 1.25rem;
+  color: #313341;
+  margin: 0;
+`;
+
+export const Subtitle = styled.h2`
+  font-weight: 400;
+  font-size: 0.5625rem;
+  color: #9a938c;
+  margin: 0;
+`;
